3959247d6f14b81eb0cbdab90fc8a5460c74bb23
Matheo Marechal <hisokaori@gmail.com>
2024-03-12 14:58:10 +0000

    ajout de l'option delete

--- vehicle/delete.go
+++ vehicle/delete.go
@@ -2,6 +2,7 @@ package vehicle
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/ToffoluttiVittorio/vehicle-server/storage"
 	"go.uber.org/zap"
@@ -20,5 +21,35 @@ func NewDeleteHandler(store storage.Store, logger *zap.Logger) *DeleteHandler {
 }
 
 func (d *DeleteHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	http.Error(rw, "Not Implemented", http.StatusInternalServerError)
+
+	id := r.PathValue("id")
+	idParse, err := strconv.ParseInt(id, 10, 64)
+
+	if err != nil {
+		d.logger.Error(
+			"Could not find vehicle from store",
+			zap.Error(err),
+		)
+		return
+	}
+
+	vehicle, err := d.store.Vehicle().Delete(r.Context(), idParse)
+
+	if err != nil {
+		d.logger.Error(
+			"Could not delete vehicle from store",
+			zap.Error(err),
+		)
+		return
+	}
+
+	if vehicle {
+		rw.WriteHeader(http.StatusNoContent)
+		return
+	} else {
+		http.Error(rw, "Id not found", http.StatusInternalServerError)
+		rw.WriteHeader(http.StatusNotFound)
+	}
+
+	// http.Error(rw, "Not Implemented", http.StatusInternalServerError)
 }
